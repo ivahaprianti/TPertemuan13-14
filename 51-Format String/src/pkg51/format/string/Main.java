4389174ecd2bc812ccddfb6fa9fc2605990091e1
muhamadhaidzirismail <rapraprap710@gmail.com>
2019-12-19 14:48:20 +0700

    TPertemuan13-14

---
+++ 51-Format String/src/pkg51/format/string/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg51.format.string;
+
+/**
+ *
+ * @author KERJAKEUN SORANGAN !
+ * Nama  : Ivah Aprianti 
+ * Kelas : TI-1A
+ */ 
+public class Main{
+
+	public static void main(String[] args){
+		System.out.println("hello world");
+	}
+} 
